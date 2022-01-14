f8b412d3fe01868fb5c8b540789c13e3cd2005eb
sojibsjoy <sojib.vu@gmail.com>
2022-01-14 19:14:08 +0600

    profile screen

--- lib/pages/profile_screen.dart
+++ lib/pages/profile_screen.dart
@@ -6,8 +6,91 @@ class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("User Profile"),
+      backgroundColor: Colors.teal,
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 70,
+              ),
+              // to make the image circular
+              ClipRRect(
+                borderRadius: BorderRadius.circular(100),
+                child: Image.asset(
+                  'assets/images/user.png',
+                  height: 200,
+                  width: 200,
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const Text(
+                'Pronob Biswas',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 55,
+                  fontFamily: 'ComforterBrush',
+                ),
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              const Text(
+                'Graphics Designer',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              infoContainer(Icons.call, '+8801721222324'),
+              const SizedBox(
+                height: 20,
+              ),
+              infoContainer(Icons.mail, 'pronobbs@gmail.com'),
+              const SizedBox(
+                height: 20,
+              ),
+              infoContainer(Icons.shop, 'Dept. of CSE'),
+              const SizedBox(
+                height: 20,
+              ),
+              infoContainer(Icons.home, 'Copy-Paste University'),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Container infoContainer(IconData icon, String text) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 35),
+      height: 50,
+      color: Colors.white54,
+      child: Row(
+        children: [
+          const SizedBox(
+            width: 25,
+          ),
+          Icon(
+            icon,
+            size: 30,
+          ),
+          const SizedBox(
+            width: 40,
+          ),
+          Text(
+            text,
+            style: const TextStyle(
+              fontSize: 20,
+            ),
+          )
+        ],
       ),
     );
   }
